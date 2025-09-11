b770dc3fa33f62bb880c7adec2f7faf99af42b4b
Thumul <thumul.dasun@gmail.com>
2025-09-11 06:50:54 +0530

    Show recruitment rounds grouped by the status

--- client/src/pages/AdminDashboard.tsx
+++ client/src/pages/AdminDashboard.tsx
@@ -1,8 +1,9 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 import RSCard from "../components/RecruitmentSeriesCard";
 import { LuCirclePlus } from "react-icons/lu";
+import { FaBoxOpen } from "react-icons/fa";
 import axiosInstance from "../api/axiosConfig";
 
 interface UserGroup {
@@ -29,7 +30,9 @@ interface RecruitmentSeriesData {
 const AdminDashboard: React.FC = () => {
   const { user } = useAuth();
   const navigate = useNavigate();
-  const [recruitmentSeriesList, setRecruitmentSeriesList] = useState<RecruitmentSeriesData[]>([]);
+  const [recruitmentSeriesList, setRecruitmentSeriesList] = useState<
+    RecruitmentSeriesData[]
+  >([]);
 
   const fetchRecruitmentSeries = async () => {
     try {
@@ -42,35 +45,100 @@ const AdminDashboard: React.FC = () => {
     }
   };
 
+  const initialisedSeries = recruitmentSeriesList.filter(
+    (series) => series.status === "initialised"
+  );
+  const publishedSeries = recruitmentSeriesList.filter(
+    (series) => series.status === "published"
+  );
+  const archivedSeries = recruitmentSeriesList.filter(
+    (series) => series.status === "archived"
+  );
+
   useEffect(() => {
     fetchRecruitmentSeries();
   }, []);
 
   return (
     <div className="min-h-screen w-full flex flex-col items-start justify-start bg-bg-page text-text-primary px-20 py-5">
-      <p className="text-3xl font-bold font-montserrat mb-5 select-none">
+      {/* <p className="text-3xl font-bold font-montserrat mb-5 select-none">
         Hi, {user?.name} 👋
-      </p>
-      <p className="text-2xl font-semibold select-none font-raleway">
-        Recruitment Series
+      </p> */}
+      <div className="flex items-center justify-between w-full">
+        <p className="text-3xl font-semibold select-none font-raleway">
+          Recruitment Rounds
+        </p>
+        <div
+          className="p-1.5 px-3 flex rounded-md gap-x-3 transition-colors duration-200 cursor-pointer group text-text-inverted bg-primary hover:bg-primary-light items-center"
+          onClick={() => navigate("/recruitment-series/create")}
+        >
+          <LuCirclePlus className="h-6 w-6" />
+          <p className="flex flex-col select-none items-center text-lg font-raleway text-nowrap text-center">
+            New Recruitment Round
+          </p>
+        </div>
+      </div>
+
+      {/* Active */}
+      <p className="mt-4 text-2xl font-semibold select-none font-raleway">
+        Active Recruitment Rounds
       </p>
       <div className="flex bg-bg-card flex-col items-center rounded-sm p-2 w-full">
-        <div className="w-full  space-y-2">
-          {recruitmentSeriesList.map((series) => (
-            <RSCard key={series._id} {...series} />
-          ))}
+        <div className="w-full space-y-2">
+          {publishedSeries.length === 0 ? (
+            <div className="w-full flex flex-col items-center justify-center py-6">
+              <FaBoxOpen className="h-8 w-8 text-text-secondary mb-2" />
+              <p className="text-lg text-text-secondary font-semibold">
+                No active Recruitment rounds at this moment.
+              </p>
+            </div>
+          ) : (
+            publishedSeries.map((series) => (
+              <RSCard key={series._id} {...series} />
+            ))
+          )}
         </div>
-        <div className="m-10 group text-text-secondary opacity-50 hover:opacity-80 hover:text-primary-light flex flex-col items-center">
-          <LuCirclePlus
-            className="h-24 w-24 cursor-pointer transition-colors duration-200"
-            onClick={() => navigate("/recruitment-series/create")}
-          />
-          <p
-            className="flex flex-col select-none items-center cursor-pointer text-lg font-raleway w-24 text-nowrap text-center"
-            onClick={() => navigate("/recruitment-series/create")}
-          >
-            New Recruitment Series
-          </p>
+      </div>
+
+      {/* Initialised */}
+      <p className="mt-4 text-2xl font-semibold select-none font-raleway">
+        Initialised Recruitment Rounds
+      </p>
+      <div className="flex bg-bg-card flex-col items-center rounded-sm p-2 w-full">
+        <div className="w-full space-y-2">
+          {initialisedSeries.length === 0 ? (
+            <div className="w-full flex flex-col items-center justify-center py-6">
+              <FaBoxOpen className="h-8 w-8 text-text-secondary mb-2" />
+              <p className="text-lg text-text-secondary font-semibold">
+                No initialised Recruitment rounds at this moment.
+              </p>
+            </div>
+          ) : (
+            initialisedSeries.map((series) => (
+              <RSCard key={series._id} {...series} />
+            ))
+          )}
+        </div>
+      </div>
+
+      {/* Archived */}
+      <p className="mt-4 text-2xl font-semibold select-none font-raleway">
+        Archived Recruitment Rounds
+      </p>
+      <div className="flex bg-bg-card flex-col items-center rounded-sm p-2 w-full">
+        <div className="w-full space-y-2">
+          {archivedSeries.length === 0 ? (
+            <div className="w-full flex flex-col items-center justify-center py-6">
+              <FaBoxOpen className="h-8 w-8 text-text-secondary mb-2" />
+              <p className="text-lg text-text-secondary font-semibold">
+                No archived Recruitment rounds at this moment.
+              </p>
+            </div>
+          ) : (
+            archivedSeries.map((series) => (
+              <RSCard key={series._id} {...series} />
+            ))
+          )}
         </div>
       </div>
     </div>
