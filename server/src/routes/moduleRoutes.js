57b85dea219bdf9b90148b9729ec456f25ab2431
Thumul <thumul.dasun@gmail.com>
2025-09-30 05:18:49 +0530

    Create a module controller for controlling module related functionalities

--- server/src/routes/moduleRoutes.js
+++ server/src/routes/moduleRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const recruitmentController = require('../controllers/recruitmentController');
+const moduleController = require('../controllers/moduleController');
 const { protected, authorize } = require('../middleware/authMiddleware');
 
-router.put('/:moduleId/change-status', protected, authorize(['admin']), recruitmentController.changeModuleStatus);
-router.get('/:moduleId', protected, authorize(['admin']), recruitmentController.getModuleDetailsById);
+router.put('/:moduleId/change-status', protected, authorize(['admin']), moduleController.changeModuleStatus);
+router.get('/:moduleId', protected, authorize(['admin']), moduleController.getModuleDetailsById);
+router.put('/:moduleId/advertise', protected, authorize(['admin']), moduleController.advertiseModule);
 
 module.exports = router;
