bc8e2cabf94d411cd696d941cf9136c81f3ceed5
Thumul <thumul.dasun@gmail.com>
2025-10-16 14:46:32 +0530

    Add CORS to the backend

--- server/server.js
+++ server/server.js
@@ -1,9 +1,29 @@
 require('dotenv').config();
 const connectDB = require('./src/config/database');
+const cors = require('cors');
 const app = require('./src/app');
 const userGroupController = require('./src/controllers/userGroupController');
 
 const PORT = process.env.PORT || 5000;
+
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+];
+
+// CORS Configuration Object
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+};
+
+// Apply the CORS middleware
+app.use(cors(corsOptions));
 
 // Connect to the database and then start the server
 connectDB().then(() => {
