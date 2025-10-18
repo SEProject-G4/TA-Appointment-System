29358590938b4815b6cbf496342ec71482f570ff
Thumul <thumul.dasun@gmail.com>
2025-10-18 17:09:48 +0530

    Edit modules feature

--- server/src/routes/moduleRoutes.js
+++ server/src/routes/moduleRoutes.js
@@ -6,5 +6,6 @@ const authMiddleware = require('../middleware/authMiddleware');
 router.put('/:moduleId/change-status', authMiddleware.protected, authMiddleware.authorize(['admin']), moduleController.changeModuleStatus);
 router.get('/:moduleId', authMiddleware.protected, authMiddleware.authorize(['admin']), moduleController.getModuleDetailsById);
 router.put('/:moduleId/advertise', authMiddleware.protected, authMiddleware.authorize(['admin']), moduleController.advertiseModule);
+router.put('/:moduleId', authMiddleware.protected, authMiddleware.authorize(['admin']), moduleController.updateModule);
 
 module.exports = router;
