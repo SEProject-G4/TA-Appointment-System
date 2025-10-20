94708babbae0a127ec4a55114e773663664f17aa
Dinara-De-Silva <dinarathis@yahoo.com>
2025-10-20 09:19:30 +0530

    removed full modules , hourfilter bug, postgrat hourlimit

--- server/src/controllers/taControllers.js
+++ server/src/controllers/taControllers.js
@@ -41,9 +41,10 @@ const getAllRequests = async (req, res) => {
       am.appliedModules.map((app) => app.moduleId)
     );
 
-    const hoursFilter = appliedModules?.[0]?.availableHoursPerWeek
+    // filter to remove that has more taHours than user has
+    const hoursFilter = appliedModules?.[0]?.availableHoursPerWeek!== undefined 
       ? { requiredTAHours: { $lte: appliedModules[0].availableHoursPerWeek } }
-      : {};
+      : userRole === "undergraduate"? { requiredTAHours: { $lte: 6 } } : { requiredTAHours: { $lte: 18 } };
 
     const modules = await ModuleDetails.find({
       recruitmentSeriesId: { $in: recSeriesIds },
@@ -51,8 +52,9 @@ const getAllRequests = async (req, res) => {
       _id: { $nin: appliedModulesIds },
       ...hoursFilter,
       ...(userRole === "undergraduate"
-        ? { openForUndergraduates: true }
-        : { openForPostgraduates: true }),
+        ? { openForUndergraduates: true, "undergraduateCounts.remaining": { $gt: 0 } }
+        : { openForPostgraduates: true, "postgraduateCounts.remaining": { $gt: 0 } }
+      ),
     });
     //fetch the available modules that have been advertised by admin.
     // and required ta hours should be less than or equal to available hours per week of the user
@@ -177,7 +179,7 @@ const applyForTA = async (req, res) => {
         recSeriesId,
         appliedModules: [taApplication._id],
         availableHoursPerWeek:
-          userRole === "undergraduate" ? 6 - taHours : 10 - taHours,
+          userRole === "undergraduate" ? 6 - taHours : 18- taHours,
       });
       await appliedModules.save({ session });
     }
