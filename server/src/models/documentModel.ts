550037b4774d7f6a2227935518d560f819589862
Dinara-De-Silva <dinarathis@yahoo.com>
2025-12-14 15:46:30 +0530

    data encryption

--- server/src/models/documentModel.ts
+++ server/src/models/documentModel.ts
@@ -1,5 +1,6 @@
 import mongoose = require("mongoose");
 import { Schema, Document, Model } from "mongoose";
+import { encrypt, decrypt } from "../utils/encryption";
 
 interface IDriveFiles {
   [fieldName: string]: {
@@ -42,6 +43,53 @@ const documentSchema = new Schema<IDocument>(
   },
   { timestamps: true }
 );
+
+// Encrypt sensitive fields before saving
+documentSchema.pre("save", function (next) {
+  try {
+    if (this.bankAccountName && !this.bankAccountName.includes(":")) {
+      this.bankAccountName = encrypt(this.bankAccountName);
+    }
+    if (this.address && !this.address.includes(":")) {
+      this.address = encrypt(this.address);
+    }
+    if (this.nicNumber && !this.nicNumber.includes(":")) {
+      this.nicNumber = encrypt(this.nicNumber);
+    }
+    if (this.accountNumber && !this.accountNumber.includes(":")) {
+      this.accountNumber = encrypt(this.accountNumber);
+    }
+    next();
+  } catch (error) {
+    next(error as Error);
+  }
+});
+
+// Decrypt sensitive fields after retrieval
+const decryptFields = function (doc: any) {
+  if (doc) {
+    if (doc.bankAccountName) doc.bankAccountName = decrypt(doc.bankAccountName);
+    if (doc.address) doc.address = decrypt(doc.address);
+    if (doc.nicNumber) doc.nicNumber = decrypt(doc.nicNumber);
+    if (doc.accountNumber) doc.accountNumber = decrypt(doc.accountNumber);
+  }
+};
+
+documentSchema.post("find", function (docs: any[]) {
+  docs.forEach(decryptFields);
+});
+
+documentSchema.post("findOne", function (doc: any) {
+  decryptFields(doc);
+});
+
+documentSchema.post("findOneAndUpdate", function (doc: any) {
+  decryptFields(doc);
+});
+
+documentSchema.post("save", function (doc: any) {
+  decryptFields(doc);
+});
 
 const documentModel: Model<IDocument> = mongoose.model<IDocument>("Document", documentSchema);
 
